6c7fe5fc8faabfe63b1ad46e00b02c7258518335
sujiiiiit <dwiwedisv@rknec.edu>
2024-07-16 11:05:47 +0000

    my new nada program

--- quickstart/client_code/run_my_first_program.py
+++ quickstart/client_code/run_my_first_program.py
@@ -1,17 +1,79 @@
-from nada_dsl import *
+class Party:
+    def __init__(self, name):
+        self.name = name
 
-def nada_main():
+class Input:
+    def __init__(self, name, party):
+        self.name = name
+        self.party = party
+
+class SecretInteger:
+    def __init__(self, value):
+        self.value = value
+    
+    def __add__(self, other):
+        return SecretInteger(self.value + other.value)
+    
+    def __mul__(self, other):
+        return SecretInteger(self.value * other.value)
+    
+    def __truediv__(self, other):
+        return SecretInteger(self.value // other.value)
+    
+    def __mod__(self, other):
+        return SecretInteger(self.value % other.value)
+    
+    def __floordiv__(self, other):
+        return SecretInteger(self.value // other.value)
+
+class Output:
+    def __init__(self, value, name, party):
+        self.value = value
+        self.name = name
+        self.party = party
+
+def sum_of_digits(n):
+    s = 0
+    while n.value > 0:
+        s += n.value % 10
+        n = SecretInteger(n.value // 10)
+    return SecretInteger(s)
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def nada_main(secret_number):
     party1 = Party(name="Alice")
     party2 = Party(name="Bob")
-    party3 = Party(name="Charlie")
     
-    x = SecretInteger(Input(name="X", party=party1))
-    y = SecretInteger(Input(name="Y", party=party2))
+    secret_number = SecretInteger(secret_number)
+    digit_sum = sum_of_digits(secret_number)
     
-    sum_result = x + y
-    product_result = x * y
+    factors = prime_factors(secret_number.value)
+    prime_factors_sum = sum(sum_of_digits(SecretInteger(pf)).value for pf in factors)
+    
+    is_boston = (digit_sum.value == prime_factors_sum)
     
     return [
-        Output(sum_result, "sum_output", party3),
-        Output(product_result, "product_output", party3)
+        Output(is_boston, "is_boston_output", party2)
     ]
+
+# Taking input from the user
+secret_number = int(input("Enter a number: "))
+
+# Running the nada_main function
+result = nada_main(secret_number)
+
+# Printing the output
+for output in result:
+    print(f"The number is a Boston number: {output.value}")
